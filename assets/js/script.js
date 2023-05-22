31f20abab092d201688bdcee57bb2a1be9e56350
aaryan200 <co21btech11001@iith.ac.in>
2023-05-22 21:34:49 +0530

    Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const toggleButton = document.getElementsByClassName('fast__navbar-toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('fast__navbar_menu')[0];
 
+// toggleButton.addEventListener('click', (e) => {
+//     e.preventDefault();
+// });
+
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active');
 });
